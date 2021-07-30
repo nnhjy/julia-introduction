a6d25c4fddbafbd3d7301bd94c6bd4503e165245
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2021-07-30 18:36:19 +0300

    Add the no-immunity version of he epidemic model

--- epidemic_no_immunity.jl
+++ epidemic_no_immunity.jl
@@ -1,5 +1,5 @@
-module epidemic
-export Cell, Parameters, create_map, to_colors, interact!, update, count_infections, count_deaths
+using Colors
+using Plots
 
 "Enumerate possible states of a single cell"
 @enum InfectionStatus uninfected infected dead recovered immune
@@ -18,23 +18,8 @@ mutable struct Parameters
     immunity_rate::Float32
 end
 
-"Create a map of immune and uninfected cells with 1 infected in the middle."
-function make_cells(width::Integer=7, height::Integer=7, immunity_rate::Float32)
-    cells = Matrix{Cell}(undef, width, height)
-    for i in 1:size(cells)[1]
-        for j in 1:size(cells)[2]
-            if rand(1)[1] < immunity_rate
-                cells[i,j] = Cell(immune, 0)
-            else
-                cells[i,j] = Cell(uninfected, 0)
-            end
-        end
-    end
-    cells[width÷2+1,height÷2+1] = 1
-end
-
 "Map the cells to colors for plotting"
-function to_colors(cell::Cell)
+function to_colors(cell)
     if cell.status == uninfected
         return RGB(0.0,0.8,0.0)
     end
@@ -47,9 +32,6 @@ function to_colors(cell::Cell)
     if cell.status == recovered
         return RGB(0.0,0.0,0.8)
     end
-    if cell.status == immune
-        return RGB(0.2,0.2,1.0)
-    end
 end
 
 """
@@ -57,7 +39,7 @@ Run the interaction between one cell and a neighbour.
 
 If the neighbour is infected, it infect this cell with the propability parameters.infection_rate.
 """
-function interact!(new_cell::Cell, other_cell::Cell, parameters::Parameters)
+function interact!(new_cell, other_cell, parameters::Parameters)
     if new_cell.status == uninfected && other_cell.status == infected
         if rand(1)[1] < parameters.infection_rate
             new_cell.status = infected
@@ -69,7 +51,7 @@ end
 """
 Update a single cell, not accounting for it's interactions with the neighbours.
 """
-function update!(new_cell::Cell, parameters::Parameters)
+function update!(new_cell, parameters::Parameters)
     if new_cell.status == infected
         # Increase the number of time steps since infection
         new_cell.infection_time += 1
@@ -86,10 +68,36 @@ function update!(new_cell::Cell, parameters::Parameters)
     end
 end
 
+"Count the current number of infections"
+function count_infections(cells::Matrix{})
+    infections = 0
+    for i in 1:size(cells)[1]
+        for j in 1:size(cells)[2]
+            if cells[i,j].status == infected
+                infections += 1
+            end
+        end
+    end
+    return infections
+end
+
+"Count the number of dead cells"
+function count_deaths(cells::Matrix{})
+    deaths = 0
+    for i in 1:size(cells)[1]
+        for j in 1:size(cells)[2]
+            if cells[i,j].status == dead
+                deaths += 1
+            end
+        end
+    end
+    return deaths
+end
+
 """
 Update the cells in the 2D array of Cells, using given parameters.
 """
-function update!(cells::Matrix{Cell}, parameters::Parameters)
+function update!(cells::Matrix{}, parameters::Parameters)
     old_cells = deepcopy(cells)
     for i in 1:size(cells)[1]
         for j in 1:size(cells)[2]
@@ -108,33 +116,4 @@ function update!(cells::Matrix{Cell}, parameters::Parameters)
             interact!(cells[i,j+1], old_cells[i,j], parameters)
         end
     end
-end
-
-"Count the current number of infections"
-function count_infections(cells::Matrix{Cell})
-    infections = 0
-    for i in 1:size(cells)[1]
-        for j in 1:size(cells)[2]
-            if cells[i,j].status == infected
-                infections += 1
-            end
-        end
-    end
-    return infections
-end
-
-"Count the number of dead cells"
-function count_deaths(cells::Matrix{Cell})
-    deaths = 0
-    for i in 1:size(cells)[1]
-        for j in 1:size(cells)[2]
-            if cells[i,j].status == dead
-                deaths += 1
-            end
-        end
-    end
-    return deaths
-end
-
-# End module
 end
